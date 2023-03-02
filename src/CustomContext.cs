9ec46c36fe3eb353ccd03dcda198e222960fa368
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2023-03-01 20:48:16 -0700

    update file header

--- src/CustomContext.cs
+++ src/CustomContext.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2022 Jason Shave. All rights reserved.
+﻿// Copyright (c) 2023 Jason Shave. All rights reserved.
 // Licensed under the MIT License.
 
 using System.Text.Json.Serialization;
@@ -8,9 +8,9 @@ namespace CallAutomation.Contracts;
 public sealed class CustomContext
 {
     [JsonPropertyName("sipHeaders")]
-    public Dictionary<string, string>? SipHeaders { get; set; } = new();
+    public Dictionary<string, string>? SipHeaders { get; set; } = new ();
 
 
     [JsonPropertyName("voipHeaders")]
-    public Dictionary<string, string>? VoIpHeaders { get; set; } = new();
+    public Dictionary<string, string>? VoIpHeaders { get; set; } = new ();
 }
